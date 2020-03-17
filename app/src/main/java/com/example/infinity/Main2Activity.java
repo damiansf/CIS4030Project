eac8e6a9e0b158a5a3ebf9626b3dd341e5b6c932
Daniel <danielkolodzi@gmail.com>
2020-03-17 19:05:18 -0400

    complete import fragment minus popping off backstack and returning selected file

--- app/src/main/java/com/example/infinity/Main2Activity.java
+++ app/src/main/java/com/example/infinity/Main2Activity.java
@@ -1,7 +1,13 @@
 package com.example.infinity;
 
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.example.infinity.ui.ImportFileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +15,11 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import android.provider.DocumentsContract;
+import android.util.Log;
+import android.view.View;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity implements ImportFileFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,4 +36,54 @@ public class Main2Activity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navView, navController);
     }
 
+    @Override
+    public void onFragmentInteraction(String fileName, String uniqueID, String fileType) {
+        String newFileName = fileName + "-" + fileType + "-" + uniqueID + ".pdf";
+        Log.i("test", newFileName);
+
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        if (fragmentManager.getBackStackEntryCount() > 0) {
+//            fragmentManager.popBackStack();
+//        }
+    }
+
+    public void importFile(View view) {
+        Fragment importFileFragment = new ImportFileFragment();
+        replaceFragment(importFileFragment);
+    }
+
+    public void replaceFragment(Fragment someFragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.nav_host_fragment, someFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    // Request code for selecting a PDF document.
+    private static final int PICK_PDF_FILE = 2;
+
+    public void openFile(View view) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("application/pdf");
+
+        // Optionally, specify a URI for the file that should appear in the
+        // system file picker when it loads.
+        //intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri);
+
+        startActivityForResult(intent, PICK_PDF_FILE);
+    }
+
+
+    public void deleteFile() {
+
+    }
+
+    public void transferFilesViaNFC() {
+
+    }
+
+    public void exportFiles() {
+
+    }
 }
---
+++ app/src/main/java/com/example/infinity/ui/ImportFileFragment.java
@@ -0,0 +1,125 @@
+package com.example.infinity.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.example.infinity.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ImportFileFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ImportFileFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ImportFileFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+//    private String mParam1;
+//    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public ImportFileFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ImportFileFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ImportFileFragment newInstance() {
+        ImportFileFragment fragment = new ImportFileFragment();
+        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View import_file_view = inflater.inflate(R.layout.fragment_import_file, container, false);
+
+        Button bt=(Button) import_file_view.findViewById(R.id.add_button);
+        bt.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String uniqueId = ((EditText)import_file_view.findViewById(R.id.unique_file_id)).getText().toString();
+                        String fileType = ((Spinner)import_file_view.findViewById(R.id.file_type)).getSelectedItem().toString();
+                        String fileName = "";
+                        if (mListener != null) {
+                            mListener.onFragmentInteraction(fileName, uniqueId, fileType);
+                        }
+                    }
+                });
+        return import_file_view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(String fileName, String uniqueID, String fileType);
+    }
+}
