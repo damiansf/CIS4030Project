bd08485cf7a7b38c769ddee725a8a35872f0b0bf
damiansf <damiansandhu11@gmail.com>
2020-03-18 22:12:48 -0400

    adding display files functionality

--- app/src/main/java/com/example/infinity/ui/manage/ManageFragment.java
+++ app/src/main/java/com/example/infinity/ui/manage/ManageFragment.java
@@ -4,15 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.infinity.MainActivity;
 import com.example.infinity.R;
+
+import java.io.File;
+import java.util.UUID;
+
 
 public class ManageFragment extends Fragment {
 
@@ -30,6 +38,19 @@ public class ManageFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        LinearLayout fileLayout = root.findViewById(R.id.fileLayout);
+        File[] externalStorageVolumes =
+                ContextCompat.getExternalFilesDirs(root.getContext(), null);
+        File primaryExternalStorage = externalStorageVolumes[0];
+        File[] files = new File(primaryExternalStorage.getPath() + "/quicklink-user-files").listFiles();
+
+        for (File file: files) {
+            CheckBox newCheck = new CheckBox(root.getContext());
+            newCheck.setText(file.getName());
+            fileLayout.addView(newCheck);
+        }
+
         return root;
     }
 }
--- app/src/main/java/com/example/infinity/ui/receive/ReceiveFragment.java
+++ app/src/main/java/com/example/infinity/ui/receive/ReceiveFragment.java
@@ -5,15 +5,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.infinity.R;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
+import java.io.File;
 
 public class ReceiveFragment extends Fragment {
     private ReceiveViewModel receiveViewModel;
@@ -28,6 +33,19 @@ public class ReceiveFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        LinearLayout fileLayout = root.findViewById(R.id.fileLayoutReceive);
+        File[] externalStorageVolumes =
+                ContextCompat.getExternalFilesDirs(root.getContext(), null);
+        File primaryExternalStorage = externalStorageVolumes[0];
+        File[] files = new File(primaryExternalStorage.getPath() + "/quicklink-user-files").listFiles();
+
+        for (File file: files) {
+            CheckBox newCheck = new CheckBox(root.getContext());
+            newCheck.setText(file.getName());
+            fileLayout.addView(newCheck);
+        }
+
         return root;
     }
 }
