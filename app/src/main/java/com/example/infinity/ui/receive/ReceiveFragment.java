abbd857c8ce0a1ea4db603d8cb0285cfb207fe12
damiansf <damiansandhu11@gmail.com>
2020-03-20 00:22:24 -0400

    adding delete file functgionaloty

--- app/src/main/java/com/example/infinity/ui/receive/ReceiveFragment.java
+++ app/src/main/java/com/example/infinity/ui/receive/ReceiveFragment.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -13,15 +15,23 @@ import com.example.infinity.R;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ReceiveFragment extends Fragment {
     private ReceiveViewModel receiveViewModel;
+    private ArrayList<String> selectedFiles = new ArrayList<>();
+    private String path;
+    private HashMap<String, Integer> fileIdMap = new HashMap<>();
+    private int count = 0;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         receiveViewModel = ViewModelProviders.of(this).get(ReceiveViewModel.class);
@@ -38,11 +48,59 @@ public class ReceiveFragment extends Fragment {
         File[] externalStorageVolumes =
                 ContextCompat.getExternalFilesDirs(root.getContext(), null);
         File primaryExternalStorage = externalStorageVolumes[0];
-        File[] files = new File(primaryExternalStorage.getPath() + "/quicklink-user-files").listFiles();
+        this.path = primaryExternalStorage.getPath();
+        File[] files = new File(path + "/quicklink-user-files").listFiles(); //TODO change this path to the transfer path
+
+        Button deleteButton = root.findViewById(R.id.btn_delete_receive);
+        deleteButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v)
+            {
+                for (String fileName: selectedFiles) {
+                    File deleteFile = null;
+                    File[] files = new File(path + "/quicklink-user-files").listFiles(); //TODO change this path to the transfer path
+
+                    for(File file: files) {
+                        if (file.getName().equals(fileName)) {
+                            deleteFile = file;
+                            break;
+                        }
+                    }
+                    if (deleteFile != null) {
+                        ConstraintLayout grandParent = (ConstraintLayout)v.getParent();
+                        LinearLayout parent = (LinearLayout) grandParent.getViewById(R.id.fileLayoutReceive);
+                        CheckBox removeBox = (CheckBox)parent.findViewById(fileIdMap.get(deleteFile.getName()));
+                        parent.removeView(removeBox);
+                        deleteFile.delete();
+                    }
+
+                }
+                selectedFiles.clear();
+            }
+        });
+
+        Button saveButton = root.findViewById(R.id.btn_save_as);
+        saveButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v)
+            {
+                //TODO
+            }
+        });
 
         for (File file: files) {
             CheckBox newCheck = new CheckBox(root.getContext());
             newCheck.setText(file.getName());
+            newCheck.setId(count);
+            fileIdMap.put(file.getName(), count);
+            count++;
+            newCheck.setOnCheckedChangeListener(new CheckBox.OnCheckedChangeListener() {
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked == true) {
+                        selectedFiles.add(buttonView.getText().toString());
+                    } else {
+                        selectedFiles.remove(buttonView.getText().toString());
+                    }
+                }
+            });
             fileLayout.addView(newCheck);
         }
 
