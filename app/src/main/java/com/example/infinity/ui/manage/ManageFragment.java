6610bf7b1b77d460898277f9d84af6357cdfd216
Jesse Millward <jesse.millward97@gmail.com>
2020-03-20 15:50:31 -0400

    Added code for file transfer

--- app/src/main/java/com/example/infinity/ui/manage/ManageFragment.java
+++ app/src/main/java/com/example/infinity/ui/manage/ManageFragment.java
@@ -10,17 +10,20 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.infinity.MainActivity;
 import com.example.infinity.R;
+import com.example.infinity.ui.transfer.TransferFragment;
 
 import java.io.File;
 import java.io.IOException;
@@ -36,6 +39,7 @@ public class ManageFragment extends Fragment {
     private String path;
     private HashMap<String, Integer> fileIdMap = new HashMap<>();
     private int count = 0;
+    private String USER_FILE_DIR = "/quicklink-user-files";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -55,7 +59,7 @@ public class ManageFragment extends Fragment {
                 ContextCompat.getExternalFilesDirs(root.getContext(), null);
         File primaryExternalStorage = externalStorageVolumes[0];
         this.path = primaryExternalStorage.getPath();
-        File[] files = new File(path + "/quicklink-user-files").listFiles();
+        File[] files = new File(path + USER_FILE_DIR).listFiles();
 
         Button deleteButton = root.findViewById(R.id.btn_delete_manage);
         deleteButton.setOnClickListener(new Button.OnClickListener() {
@@ -63,7 +67,7 @@ public class ManageFragment extends Fragment {
             {
                 for (String fileName: selectedFiles) {
                     File deleteFile = null;
-                    File[] files = new File(path + "/quicklink-user-files").listFiles();
+                    File[] files = new File(path + USER_FILE_DIR).listFiles();
 
                     for(File file: files) {
                         if (file.getName().equals(fileName)) {
@@ -83,6 +87,28 @@ public class ManageFragment extends Fragment {
             }
         });
 
+        Button transferButton = root.findViewById(R.id.btn_transfer);
+        transferButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+
+                // If no files selected for transfer, do nothing
+                if (selectedFiles.size() < 1) {
+                    Toast.makeText(getActivity().getApplicationContext(), "Need to select files to transfer", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // Transition to the transfer fragment, passing the selected file paths as an argument
+                TransferFragment fragment = new TransferFragment();
+                Bundle arguments = new Bundle();
+                arguments.putStringArrayList("filePaths", selectedFiles);
+                arguments.putString("basePath", path);
+                arguments.putString("userFileDir", USER_FILE_DIR);
+                fragment.setArguments(arguments);
+                FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+                transaction.addToBackStack(null);
+                transaction.replace(R.id.fragment_container, fragment);
+                transaction.commit();
+            }
+        });
 
         for (File file: files) {
             CheckBox newCheck = new CheckBox(root.getContext());
@@ -103,5 +129,11 @@ public class ManageFragment extends Fragment {
         }
 
         return root;
+    }
+
+    public void replaceFragment(Fragment someFragment) {
+        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, someFragment);
+        transaction.commit();
     }
 }
--- app/src/main/java/com/example/infinity/ui/transfer/TransferFragment.java
+++ app/src/main/java/com/example/infinity/ui/transfer/TransferFragment.java
@@ -1,10 +1,15 @@
 package com.example.infinity.ui.transfer;
 
+import android.net.Uri;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,9 +19,18 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.infinity.R;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class TransferFragment extends Fragment {
 
     private TransferViewModel galleryViewModel;
+    private ArrayList<String> filesToTransfer = new ArrayList<>();
+
+    private NfcAdapter nfc = null;
+
+    private String baseDir;
+    private String userFileDir;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +44,38 @@ public class TransferFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        Bundle arguments = getArguments();
+        filesToTransfer = arguments.getStringArrayList("filePaths");
+        baseDir = arguments.getString("basePath");
+        userFileDir = arguments.getString("userFileDir");
+
+        // Set up the message to display files being transferred
+        String outStr = "Ready to transfer; touch your phone to another to transfer the following documents:\n";
+        for (String fileName: filesToTransfer) {
+            outStr += fileName + "\n";
+        }
+        final TextView outTxt = root.findViewById(R.id.txt_out);
+
+        // Set up the nfc adapter
+        nfc = NfcAdapter.getDefaultAdapter(getContext());
+        nfc.setOnNdefPushCompleteCallback(new NfcAdapter.OnNdefPushCompleteCallback() {
+            @Override
+            public void onNdefPushComplete(NfcEvent event) {
+                // Tell the user the transfer has been initiated
+                outTxt.setText("Transfer has been started.  Tap your phone to another phone to begin additional transfers, or press the back button to return to the previous screen");
+            }
+        }, getActivity());
+        Uri[] fileUris = new Uri[filesToTransfer.size()];
+        for(int i = 0; i < filesToTransfer.size(); i++) {
+            String fileStr = "file:/" + new File(baseDir + userFileDir + "/" + filesToTransfer.get(i)).toString();
+            Uri temp = Uri.parse(fileStr);
+            fileUris[i] = temp;
+        }
+        nfc.setBeamPushUris(fileUris, getActivity());
+
+        outTxt.setText(outStr);
+
         return root;
     }
 }
